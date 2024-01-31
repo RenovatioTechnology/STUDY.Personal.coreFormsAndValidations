d15839cbf3ae96cb42ffe8f63b06fde0fca2f687
Jefferson Nah Jr <J-Nah@outlook.com>
2024-01-30 23:19:59 -0500

    Weakly Typed Forms

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace coreFormsAndValidations.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult WeeklyLogin()
+        {
+            return View();
+        }
+        // Post Method
+        [HttpPost]
+        // weekly type,whatever property you are associating here,in our case from the "WeeklyLogin View" the two string values will be taken here. One is the username and one is the password. 
+        public IActionResult Loginpost(string username,string password)
+        {
+            ViewBag.Username = username;    
+            ViewBag.Password = password;    
+            return View();
+        }
+    }
+}
